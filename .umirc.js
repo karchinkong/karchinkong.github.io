7bd7be8f1710eb8a37fb6612b744d2ed452b439c
邝嘉俊 <kuangjiajun@chinasie.com>
2019-08-16 17:33:16 +0800

    update [ 大改版 ]

---
+++ .umirc.js
@@ -0,0 +1,28 @@
+export default {
+    treeShaking: true,
+    plugins: [
+        ['umi-plugin-react', {
+            antd: true,
+            dva: true,
+            dynamicImport: true,
+            title: 'karchinkong.github.io',
+            dll: true
+        }],
+    ],
+    history: 'hash',
+    publicPath: './',
+    routes: [
+        {
+            path: '/',
+            redirect: '/app',
+        },
+        {
+            path: '/app',
+            component: '../layouts/index',
+            routes: [
+                { path: '/app', component: './index' },
+                { path: '/app/about', component: './about/about' }
+            ],
+        },
+    ],
+};
