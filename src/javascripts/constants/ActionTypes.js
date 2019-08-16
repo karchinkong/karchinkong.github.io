d0b58bc370bc39428b4fa4bbd7847834c2583aeb
邝嘉俊 <kuangjiajun@chinasie.com>
2019-08-16 17:24:34 +0800

    update [ 大改版 ]

--- src/javascripts/constants/ActionTypes.js
+++
@@ -1,3 +0,0 @@
-export const RECEIVE_ISSUES = 'RECEIVE_ISSUES';
-
-export const FETCH_INFO = 'FETCH_INFO';
--- src/javascripts/constants/Config.js
+++
@@ -1,9 +0,0 @@
-export const CONFIG = {
-	owner: 'karchinkong',
-	repo: 'BlogSystem',
-	title: 'karchin - 写给未来的自己',
-	loadingTitle: 'karchin - 正在加载...',
-	githubUrl: 'https://github.com/karchinkong',
-	jobs: 'Web Developer',
-	age: 23
-};
