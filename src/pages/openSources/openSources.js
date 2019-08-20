5a65cdcdb44c59684400f222a13570ed2738dd8b
邝嘉俊 <kuangjiajun@chinasie.com>
2019-08-20 18:41:12 +0800

    update

--- src/pages/openSources/openSources.js
+++ src/pages/openSources/openSources.js
@@ -1,15 +1,29 @@
-import React, { useEffect } from 'react';
-import { Icon, Empty } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Icon, Empty, Modal, Row, Col, Tooltip } from 'antd';
 import { connect } from 'dva';
 import styles from './openSources.less';
 import Title from '../../components/Title/Title';
 import ListWithLoading from '../../components/listWithLoading/listWithLoading';
+import moment from 'moment';
 
 const OpenSources = (props) => {
+
+    const [detailModal, setDetailModal] = useState(false);
+    const [detail, setDetail] = useState();
 
     useEffect(() => {
         props.dispatch({ type: 'index/queryOpenSourceLists' });
     }, []);
+
+    const showDetailModal = item => {
+        console.log(item);
+        setDetail(item);
+        setDetailModal(true);
+    };
+
+    const closeDetailModal = () => {
+        setDetailModal(false);
+    };
 
     const { openSourceLists, loading } = props;
 
@@ -24,7 +38,7 @@ const OpenSources = (props) => {
                     <ListWithLoading loading={loading}>
                         {
                             openSourceLists && openSourceLists.length > 0 ? openSourceLists.map(item => (
-                                <section className={styles.list} key={item.id}>
+                                <section className={styles.list} key={item.id} onClick={() => showDetailModal(item)}>
                                     <h1>{item.name}</h1>
                                     <p className={styles.description}>{item.description}</p>
                                     <div className={styles.info}>
@@ -47,6 +61,43 @@ const OpenSources = (props) => {
                         }
                     </ListWithLoading>
                 </div>
+                <Modal
+                    title={detail && detail.name}
+                    visible={detailModal}
+                    onOk={closeDetailModal}
+                    onCancel={closeDetailModal}
+                >
+                    {
+                        detail && Object.keys(detail).length > 0 && (
+                            <div className={styles.modalWrapper}>
+                                <Row>
+                                    <Col span={5}>作者</Col>
+                                    <Col span={19}>{detail.owner.login}</Col>
+                                </Row>
+                                <Row>
+                                    <Col span={5}>创建时间</Col>
+                                    <Col span={19}>
+                                        <Tooltip title={moment(detail.created_at).format('YYYY-MM-DD HH:mm:ss')}>
+                                            {moment(detail.created_at).fromNow()}
+                                        </Tooltip>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col span={5}>更新时间</Col>
+                                    <Col span={19}>
+                                        <Tooltip title={moment(detail.updated_at).format('YYYY-MM-DD HH:mm:ss')}>
+                                            {moment(detail.updated_at).fromNow()}
+                                        </Tooltip>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col span={5}>浏览地址</Col>
+                                    <Col span={19}><a target="_blank" href={detail.html_url}>{detail.html_url}</a></Col>
+                                </Row>
+                            </div>
+                        )
+                    }
+                </Modal>
             </article>
         </main>
     );
